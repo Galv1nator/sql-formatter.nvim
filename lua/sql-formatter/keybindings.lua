22a81ff4dec78d25ceb0573958142a87f6d4dfe7
abc <abc@mail.com>
2026-06-14 21:22:33 +0200

    remove remnant from nonglobal config

--- lua/sql-formatter/keybindings.lua
+++ lua/sql-formatter/keybindings.lua
@@ -10,26 +10,26 @@ function M.setup()
     return
   end
 
-  M.setup_global_keymaps(vim.g.sql_formatter_keybindings)
+  M.setup_global_keymaps()
   M.setup_buffer_keymaps()
 end
 
-function M.setup_global_keymaps(keybindings)
-  if keybindings.format_buffer then
-    vim.keymap.set("n", keybindings.format_buffer, function()
+function M.setup_global_keymaps()
+  if vim.g.sqlformatter_keybindings.format_buffer then
+    vim.keymap.set("n", vim.g.sqlformatter_keybindings.format_buffer, function()
       formatter.format_buffer()
     end, { desc = "Format SQL buffer", silent = true })
   end
 
-  if keybindings.format_selection then
-    vim.keymap.set("v", keybindings.format_selection, function()
+  if vim.g.sqlformatter_keybindings.format_selection then
+    vim.keymap.set("v", vim.g.sqlformatter_keybindings.format_selection, function()
       local start_line, end_line = utils.get_visual_selection()
       formatter.format_range(start_line, end_line)
     end, { desc = "Format SQL selection", silent = true })
   end
 
-  if keybindings.toggle_format_on_save then
-    vim.keymap.set("n", keybindings.toggle_format_on_save, function()
+  if vim.g.sqlformatter_keybindings.toggle_format_on_save then
+    vim.keymap.set("n", vim.g.sqlformatter_keybindings.toggle_format_on_save, function()
       require("sql-formatter.commands").toggle_format_on_save()
     end, { desc = "Toggle SQL format on save", silent = true })
   end
